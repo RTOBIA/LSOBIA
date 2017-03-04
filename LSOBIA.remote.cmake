41c0c76a8e3800ee4239b16c473e986f9a316a57
RTOBIA <arnaud.mallen@thales-services.fr>
2017-03-04 15:22:51 +0100

    ENH: Enabling remote module

--- LSOBIA.remote.cmake
+++ LSOBIA.remote.cmake
@@ -2,5 +2,5 @@
 otb_fetch_module(LSOBIA
         "Large scale object based image analysis module"
         GIT_REPOSITORY https://github.com/RTOBIA/LSOBIA.git
-        GIT_TAG TOFILL
+        GIT_TAG "master"
         )
